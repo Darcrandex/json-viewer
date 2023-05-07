f7d52e42fd35960e5f6e3210926587a89e8950fd
darcrand <darcrandex@gmail.com>
2023-05-07 03:51:51 +0000

    🔧 build(依赖): 放弃兼容性处理

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
@@ -7,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, './')
 
   return {
-    plugins: [react(), legacy()],
+    plugins: [react()],
     resolve: { alias: { '@': path.resolve(__dirname, './src'), src: path.resolve(__dirname, './src') } },
     server: { host: true, port: parseInt(env.VITE_APP_PORT) || 3000 },
   }
