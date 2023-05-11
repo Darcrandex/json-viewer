6da360d91f9ac1ee92715bd2aaecc69fa386adb8
darcrand <darcrandex@gmail.com>
2023-05-11 18:11:56 +0800

    🎈 perf: 将配置项保存到 storage

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -1,10 +1,11 @@
-import { atom, useAtom } from 'jotai'
+import { useAtom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 
 export type Config = { fontSize?: number; theme?: string }
 
 // 代码主题参考 https://github.com/ajaxorg/ace/tree/master/src/theme
 // 同时需要预先引入文件
-const stateAtom = atom<Config>({ fontSize: 22, theme: 'ace/theme/nord_dark' })
+const stateAtom = atomWithStorage<Config>('code-config', { fontSize: 22, theme: 'ace/theme/nord_dark' })
 
 export function useConfig() {
   const [config, setConfig] = useAtom(stateAtom)
