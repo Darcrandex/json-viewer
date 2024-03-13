795719c3231a3dc594efb75037752ba120c1f351
darcrand <darcrandex@gmail.com>
2024-03-13 17:04:35 +0800

    🎉 init: just do it again

--- src/ui/Popover/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as PopoverContent } from './Content'
-export { default as PopoverRoot } from './Root'
-export { default as PopoverTrigger } from './Trigger'
-
-export * from './types'
--- src/ui/Popover/types.ts
+++
@@ -1,6 +0,0 @@
-export type PopoverContextValue = {
-  open: boolean
-  setOpen: (open: boolean) => void
-
-  ref: React.RefObject<HTMLButtonElement>
-}
