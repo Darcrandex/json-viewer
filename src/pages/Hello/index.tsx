4f6ae2e2f5c648c3ef8237a1011ac534ddcc9951
darcrand <darcrandex@gmail.com>
2024-04-05 11:08:14 +0800

    ✨ feat(main): updaet logo

--- src/pages/Hello/index.tsx
+++ src/pages/Hello/index.tsx
@@ -8,12 +8,7 @@ export default function Hello() {
   return (
     <>
       <section className='flex flex-col items-center justify-center h-[60vh]'>
-        <span
-          className='text-gray-100'
-          style={{ fontFamily: 'Monoton-Regular', fontSize: '100px' }}
-        >
-          &#123;&nbsp;&#125;
-        </span>
+        <img src='/favicon.png' alt='' className='block w-48' />
       </section>
     </>
   )
